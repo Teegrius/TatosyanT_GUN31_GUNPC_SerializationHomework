ea5767836b275a0d5880ce8e697d87801be68065
Teegrius <tatosian.tigr@yandex.ru>
2025-11-24 21:16:18 +0300

    Updated PositionSaver

--- Assets/Scripts/PositionSaver.cs
+++ Assets/Scripts/PositionSaver.cs
@@ -5,60 +5,65 @@ using UnityEngine;
 
 namespace DefaultNamespace
 {
-	public class PositionSaver : MonoBehaviour
-	{
-		public struct Data
-		{
-			public Vector3 Position;
-			public float Time;
-		}
+    public class PositionSaver : MonoBehaviour
+    {
+        [System.Serializable]
+        public struct Data
+        {
+            public Vector3 Position;
+            public float Time;
+        }
 
-		private TextAsset _json;
+        [ReadOnly]
+        [Tooltip("Для заполнения воспользуйтесь контекстным меню - 'Create File'")]
+        private TextAsset _json;
 
-		public List<Data> Records { get; private set; }
+        [HideInInspector]
+        [SerializeField]
+        public List<Data> Records { get; private set; }
 
-		private void Awake()
-		{
+        private void Awake()
+        {
             //todo comment: Что будет, если в теле этого условия не сделать выход из метода?
             // ОТВЕТ: Будет вызвана ошибка JsonUtility.FromJsonOverwrite на null reference, так как _json = null
             if (_json == null)
-			{
-				gameObject.SetActive(false);
-				Debug.LogError("Please, create TextAsset and add in field _json");
-				return;
-			}
-			
-			JsonUtility.FromJsonOverwrite(_json.text, this);
+            {
+                gameObject.SetActive(false);
+                Debug.LogError("Please, create TextAsset and add in field _json");
+                return;
+            }
+
+            JsonUtility.FromJsonOverwrite(_json.text, this);
             //todo comment: Для чего нужна эта проверка (что она позволяет избежать)?
             // ОТВЕТ: Чтобы избежать NullReferenceException если десериализация не создала список Records
             if (Records == null)
-				Records = new List<Data>(10);
-		}
+                Records = new List<Data>(10);
+        }
 
-		private void OnDrawGizmos()
-		{
+        private void OnDrawGizmos()
+        {
             //todo comment: Зачем нужны эти проверки (что они позволляют избежать)?
             // ОТВЕТ: Чтобы избежать ошибок при отрисовке Gizmos если список пуст или не инициализирован
             if (Records == null || Records.Count == 0) return;
-			var data = Records;
-			var prev = data[0].Position;
-			Gizmos.color = Color.green;
-			Gizmos.DrawWireSphere(prev, 0.3f);
+            var data = Records;
+            var prev = data[0].Position;
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(prev, 0.3f);
             //todo comment: Почему итерация начинается не с нулевого элемента?
             // ОТВЕТ: Потому что первый элемент уже обработан до цикла (prev = data[0]), 
             for (int i = 1; i < data.Count; i++)
-			{
-				var curr = data[i].Position;
-				Gizmos.DrawWireSphere(curr, 0.3f);
-				Gizmos.DrawLine(prev, curr);
-				prev = curr;
-			}
-		}
-		
+            {
+                var curr = data[i].Position;
+                Gizmos.DrawWireSphere(curr, 0.3f);
+                Gizmos.DrawLine(prev, curr);
+                prev = curr;
+            }
+        }
+
 #if UNITY_EDITOR
-		[ContextMenu("Create File")]
-		private void CreateFile()
-		{
+        [ContextMenu("Create File")]
+        private void CreateFile()
+        {
             //todo comment: Что происходит в этой строке?
             // ОТВЕТ: Создаётся файл "Path.txt" в папке Assets проекта
             var stream = File.Create(Path.Combine(Application.dataPath, "Path.txt"));
@@ -66,33 +71,33 @@ namespace DefaultNamespace
             // ОТВЕТ: Освобождает файловый поток, чтобы файл стал доступен для работы Unity
             // Без этого AssetDatabase не сможет работать с файлом (файл заблокирован)
             stream.Dispose();
-			UnityEditor.AssetDatabase.Refresh();
-			//В Unity можно искать объекты по их типу, для этого используется префикс "t:"
-			//После нахождения, Юнити возвращает массив гуидов (которые в мета-файлах задаются, например)
-			var guids = UnityEditor.AssetDatabase.FindAssets("t:TextAsset");
-			foreach (var guid in guids)
-			{
-				//Этой командой можно получить путь к ассету через его гуид
-				var path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
-				//Этой командой можно загрузить сам ассет
-				var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<TextAsset>(path);
+            UnityEditor.AssetDatabase.Refresh();
+            //В Unity можно искать объекты по их типу, для этого используется префикс "t:"
+            //После нахождения, Юнити возвращает массив гуидов (которые в мета-файлах задаются, например)
+            var guids = UnityEditor.AssetDatabase.FindAssets("t:TextAsset");
+            foreach (var guid in guids)
+            {
+                //Этой командой можно получить путь к ассету через его гуид
+                var path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+                //Этой командой можно загрузить сам ассет
+                var asset = UnityEditor.AssetDatabase.LoadAssetAtPath<TextAsset>(path);
                 //todo comment: Для чего нужны эти проверки?
                 // ОТВЕТ: Чтобы найти именно TextAsset с именем "Path" и избежать NullReferenceException
                 if (asset != null && asset.name == "Path")
-				{
-					_json = asset;
-					UnityEditor.EditorUtility.SetDirty(this);
-					UnityEditor.AssetDatabase.SaveAssets();
-					UnityEditor.AssetDatabase.Refresh();
+                {
+                    _json = asset;
+                    UnityEditor.EditorUtility.SetDirty(this);
+                    UnityEditor.AssetDatabase.SaveAssets();
+                    UnityEditor.AssetDatabase.Refresh();
                     //todo comment: Почему мы здесь выходим, а не продолжаем итерироваться?
                     // ОТВЕТ: Потому что нужный файл уже найден, нет смысла продолжать поиск
                     return;
-				}
-			}
-		}
+                }
+            }
+        }
 
-		private void OnDestroy()
-		{
+        private void OnDestroy()
+        {
 #if UNITY_EDITOR
             // ОТВЕТ: Сохраняем данные Records в JSON файл при уничтожении объекта
             if (_json != null && Records != null && Records.Count > 0)
@@ -110,6 +115,6 @@ namespace DefaultNamespace
             }
 #endif
         }
-    }
 #endif
+    }
 }
